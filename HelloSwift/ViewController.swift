5afaa506599c027d2344123bedb4d281d5ea118e
Perry Gabriel <prgabriel88@gmail.com>
2016-10-28 14:28:57 -0400

    Completed project

--- HelloSwift/ViewController.swift
+++ HelloSwift/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+	
+	@IBOutlet weak var helloButton: UIButton!
 
+	@IBAction func showAlert(_ sender: AnyObject) {
+		let alert = UIAlertController(title: "Hello!", message: "Hello, World!", preferredStyle: UIAlertControllerStyle.alert)
+		alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil))
+		self.present(alert, animated: true, completion: nil)
+		self.helloButton.setTitle("Test!", for: UIControlState.normal)
+	}
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
